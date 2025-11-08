9396808289d23086e47f588673985af10ab0558f
Roland Tanglao <roland@rolandtanglao.com>
2025-11-07 21:27:05 -0800

    links_blog_all_open.rb seems to work :-)

--- linkless_blog_all_open.rb
+++ linkless_blog_all_open.rb
@@ -9,6 +9,7 @@ require 'pry'
 require 'typhoeus'
 require 'json'
 require 'uri'
+require 'parseconfig'
 
 logger = Logger.new($stderr)
 logger.level = Logger::DEBUG
@@ -16,9 +17,9 @@ logger.level = Logger::DEBUG
 # credentials="somebody@example.com:Open API key e.g. kdkdkdk3%" <- api key from https://checkvist.com/auth/profile#status
 # encoded_credentials = Base64.strict_encode64(credentials)
 # authorization_header = "Basic #{encoded_credentials}"
-
-
-def get_checkvist_response(url, params, logger)
+checkvist_config = ParseConfig.new('checkvist.conf').params
+basic_auth  = checkvist_config['basic_auth']
+def get_checkvist_response(url, params, basic_auth, logger)
   logger.debug url
   logger.debug params
   try_count = 0
@@ -27,7 +28,7 @@ def get_checkvist_response(url, params, logger)
       url,
       method: :get,
       headers: {
-        'Authorization' => ''
+        'Authorization' => basic_auth
       }
     )
 
@@ -50,7 +51,7 @@ def get_checkvist_response(url, params, logger)
   x
 end
 
-all_tasks = get_checkvist_response('https://checkvist.com/checklists/936619/tasks.json', '', logger)
+all_tasks = get_checkvist_response('https://checkvist.com/checklists/936619/tasks.json', '', basic_auth, logger)
 logger.debug "all_tasks: #{all_tasks.ai}"
 all_tasks.each do |t|
   logger.debug "t: #{t.ai}"
@@ -64,6 +65,6 @@ all_tasks.each do |t|
   next if yes_no.downcase == 'n'
 
   puts "let's do this"
-  system_str = "./blogthis.rb https://checkvist.com/checklists/936619/tasks/#{t['id']}"
+  system_str = "./without-link-blogthis.rb https://checkvist.com/checklists/936619/tasks/#{t['id']}"
   system(system_str)
 end
